98d5863e7048dd1b915646188cf9c45923a6311d
JPaps <jpapon2@gmail.com>
2018-04-13 10:29:34 +0200

    kdo marko

---
+++ app/src/main/java/com/example/jeanp/applilol/Activity2.java
@@ -0,0 +1,25 @@
+package com.example.jeanp.applilol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        Button Retour = (Button) findViewById(R.id.Retour);
+
+
+
+    }
+
+
+
+
+}
